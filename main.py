11830a3ccae3ecd85f5a4ffc53883b5172a57cb4
Madina-wq-ops <2286892758@qq.com>
2025-05-09 17:09:44 +0800

    First commit: print welcome message

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ print("Welcome to my simple Python program!")
 def add(a, b):
     return a + b
 
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
-print(f"{x} + {y} = {add(x, y)}")
+print("2 + 3 =", add(2, 3))
