9ebc691247cf88cdc543b75e75eec7a337ac6100
Madina-wq-ops <2286892758@qq.com>
2025-05-10 16:47:44 +0800

    Second commit: add addition function

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ print("Welcome to my simple Python program!")
 def add(a, b):
     return a + b
 
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-print(x, "+", y, "=", add(x, y))
+print("2 + 3 =", add(2, 3))
+
+
 
 
 
